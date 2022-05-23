af39c6a7cc0d1f2e604acc0a98149d93d3681bd2
Koudocko <tyler@pcquest.ca>
2022-05-22 20:33:56 -0400

    Added Arduino lib. Renamed proj folder.

--- src/btn.h
+++ src/btn.h
@@ -1,6 +1,8 @@
 #ifndef BTN_H
 #define BTN_H
 
+#include <Arduino.h>
+
 class Button{
 public:
     Button(unsigned long pin, unsigned long delay, void (*func)()) : pinNum{pin}, pushDelay{delay}, callback{func}{}
